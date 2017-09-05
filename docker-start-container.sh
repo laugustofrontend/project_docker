fdc42308f6f4b692dc2386b335c2b504ca4e2921
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-09-04 21:17:19 -0300

    initial files

---
+++ docker-start-container.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+docker start swordartonline_mysql_1 swordartonline_phpmyadmin_1 swordartonline_php_1 swordartonline_nginx_1
+docker ps
---
+++ docker-stop-container.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+docker stop swordartonline_mysql_1 swordartonline_phpmyadmin_1 swordartonline_php_1 swordartonline_nginx_1
+docker ps
